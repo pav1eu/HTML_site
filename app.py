5c2a9b92cd1aefb7e389740187c0c3cd8ef8e46b
Pavel Artiev <arijev@bk.ru>
2025-03-27 15:39:56 +0300

    fixes

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
-
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 hostName = "localhost"
 serverPort = 8080
 
-
-class MyServer(BaseHTTPRequestHandler):
-
+class MyServer(SimpleHTTPRequestHandler):
     def do_GET(self):
-        """Обработка  GET запросов"""
-        self.send_response(200)
-        self.send_header("content-type", "text/html")
-        self.end_headers()
-        with open("index.html", "r", encoding="utf-8") as file:
-            page = file.read()
-        self.wfile.write(bytes(page, "utf-8"))
+        # Если запрашивается корневой URL, отдаем index.html
+        if self.path == "/":
+            self.path = "/index.html"
+        return super().do_GET()
 
+    def end_headers(self):
+        # Устанавливаем правильные MIME-типы для CSS, JS и других файлов
+        if self.path.endswith(".css"):
+            self.send_header("Content-Type", "text/css")
+        elif self.path.endswith(".js"):
+            self.send_header("Content-Type", "application/javascript")
+        elif self.path.endswith(".html"):
+            self.send_header("Content-Type", "text/html")
+        elif self.path.endswith(".json"):
+            self.send_header("Content-Type", "application/json")
+        elif self.path.endswith(".svg"):
+            self.send_header("Content-Type", "image/svg+xml")
+        elif self.path.endswith(".png"):
+            self.send_header("Content-Type", "image/png")
+        elif self.path.endswith(".jpg") or self.path.endswith(".jpeg"):
+            self.send_header("Content-Type", "image/jpeg")
+        elif self.path.endswith(".gif"):
+            self.send_header("Content-Type", "image/gif")
+        super().end_headers()
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started at http://%s:%s" % (hostName, serverPort))
+    print(f"Server started at http://{hostName}:{serverPort}")
 
     try:
         webServer.serve_forever()
